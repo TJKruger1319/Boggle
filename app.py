bd10b78f18accd9d35399023fcb211f25cc51d72
TJ <TJKruger1319@gmail.com>
2023-04-30 21:49:26 -0400

    Adds scoreboard

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, session, render_template, jsonify, redirect, request, flash
+from flask import Flask, session, render_template, jsonify, request
 from boggle import Boggle
 
 boggle_game = Boggle()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'An open book'
+app.config['SECRET_KEY'] = 'An open book '
 
 new_boggle = Boggle()
 board = new_boggle.make_board()
@@ -15,13 +15,22 @@ def start_game():
     session["board"] = board
     return render_template("board.html", board=board)
 
+@app.route("/getguess", methods=['POST'])
+def get_guess():
+    word = request.json
+    session['word'] = word['value']
+    print(word, session['word'], "38")
+    if word == None:
+        return "Failure"
+    else:
+        return f"Success, your word is {session['word']}"
 
 @app.route("/guess", methods=['GET','POST'])
 def is_word():
     session["result"] = ""
     board = session["board"]
     word = session['word']
-    print(word, "24")
+    print(word, session['word'], "24")
     answer = new_boggle.check_valid_word(board, word)
     dict = {"result": answer}
     session["result"] = dict
@@ -30,9 +39,3 @@ def is_word():
     return result
 
 
-@app.route("/getguess", methods=['POST'])
-def get_guess():
-    word = request.form.get('boggle-guess')
-    session['word'] = word
-    print(word, "36")
-    return "Success"
--- static/word.js
+++ static/word.js
@@ -1,17 +1,35 @@
 const $guessForm = $("#guess-form");
 const $guess = $("#guess");
 const BASE_URL = "http://127.0.0.1:5000";
+let score = 0;
 
-async function getFromServer(){
+async function getFromServer(value){
+    //Gets the result from the backend
     let test = await axios.get(`${BASE_URL}/guess`);
-    console.log(test.data.result);
-    $('#result').text(test.data.result)
+    result = test.data.result
+    console.log(result);
+    displayResult(result);
+    scoreBoard(result, value)
 }
 
 async function sendToServer(value){
+    //Sends the guess to the backend
     return await axios.post(`${BASE_URL}/getguess`, {
         value: value
     })
+}
+
+function displayResult(result) {
+    //Put the result on the ui
+    $('#result').text(result);
+}
+
+function scoreBoard(result, value){
+    //Determines if the word is in the boggle, and then updates the score if it is
+    if (result === "ok") {
+        score += value.length;
+        $('#score').text(`Score: ${score}`)
+    }
 }
 
 $guessForm.on("submit", function(e) {
@@ -19,6 +37,6 @@ $guessForm.on("submit", function(e) {
     let value = $("#guess").val();
     console.log(value);
     sendToServer(value);
-    getFromServer();
+    getFromServer(value);
 });
 
