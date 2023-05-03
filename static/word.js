70862cadd77b93e400124ad85f398df15f31c17b
TJ <TJKruger1319@gmail.com>
2023-05-03 15:14:50 -0400

    Adds start and end of the game

--- static/word.js
+++ static/word.js
@@ -1,7 +1,14 @@
 const $guessForm = $("#guess-form");
+const $board = $('#board');
 const $guess = $("#guess");
+const $start = $(".start");
+const $startSection = $("#start-section");
+const $resultSection = $("#result-section");
+const $finalSection = $("#final-section")
 const BASE_URL = "http://127.0.0.1:5000";
 let score = 0;
+let highestScore = 0;
+let timesPlayed = 0;
 
 async function getFromServer(value){
     //Gets the result from the backend
@@ -9,15 +16,10 @@ async function getFromServer(value){
         value: value
     })
     result = test.data.result
-    console.log(result);
-    displayResult(result);
-    scoreBoard(result, value)
-}
-
-
-function displayResult(result) {
+    console.log(`and the result of that is ${result}`);
     //Put the result on the ui
     $('#result').text(result);
+    scoreBoard(result, value)
 }
 
 function scoreBoard(result, value){
@@ -28,10 +30,54 @@ function scoreBoard(result, value){
     }
 }
 
+function statistics() {
+    //Shows your final score, your highest score, and the amount of times you played
+    if (score >= highestScore) {
+        highestScore = score;
+    }
+    timesPlayed++;
+    $('#final-score').text(`Final score: ${score}`)
+    $('#high-score').text(`Your highest score: ${highestScore}`)
+    $('#times-played').text(`Times played: ${timesPlayed}`)
+}
+
+function startGame() {
+    //Resets base values if game is being replayed
+    score = 0;
+    $('#result').text('');
+    $('#score').text(`Score: 0`)
+
+    //Shows the game board
+    $startSection.hide();
+    $board.show();
+    $guessForm.show();
+    $resultSection.show();
+
+    //Minute timer for when game ends
+    setTimeout(endGame, 60000);
+}
+
+function endGame() {
+    //Removes the game board
+    $board.hide();
+    $guessForm.hide();
+    $resultSection.hide();
+    $finalSection.show();
+
+    //Shows the final statistics
+    statistics();
+}
+
+$start.on("click", function(){
+    $finalSection.hide();
+    startGame();
+})
+
 $guessForm.on("submit", function(e) {
     e.preventDefault();
     let value = $("#guess").val();
-    console.log(value);
+    console.log(`The word entered was: ${value}`);
     getFromServer(value);
+    $guessForm.trigger("reset");
 });
 
