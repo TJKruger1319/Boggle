01ef4d4de09211fd2b614d719c1d2d7170ffb97c
TJ <TJKruger1319@gmail.com>
2023-05-04 21:23:04 -0400

    added tests

--- tests/test.py
+++ tests/test.py
@@ -6,5 +6,22 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def test_post(self):
+        with app.test_client() as client:
+            client.get("/")
+            resp = client.post("/guess", data={"value": "c"})
+            var = resp.get_data(as_text=True)
 
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(var, "not-on-board" or "ok")
+
+    def test_extra_word(self):
+        with app.test_client() as client:
+             with client.session_transaction() as change_session:
+                client.get("/")
+                change_session['guesses'] = ["c"]      
+                resp = client.post("/guess", data={"value": "c"})
+                var = resp.get_data(as_text=True)
+
+                self.assertEqual(resp.status_code, 200)
+                self.assertEqual(var, "already-guessed")
